3159035e9e4df23719e38dc33ced25dbbbed430b
kur11208 <piromasa1234@gmail.com>
2026-06-04 14:47:30 +0900

    Update deploy_phone_key.cmd

--- etf_mobile/deploy_phone_key.cmd
+++ etf_mobile/deploy_phone_key.cmd
@@ -2,9 +2,9 @@
 setlocal
 
 if not "%~1"=="" set "ETF_PHONE_HOST=%~1"
-if "%ETF_PHONE_HOST%"=="" set "ETF_PHONE_HOST=192.168.0.96"
-if "%ETF_PHONE_USER%"=="" set "ETF_PHONE_USER=u0_a31"
-if "%ETF_PHONE_KEY%"=="" set "ETF_PHONE_KEY=%USERPROFILE%\.ssh\etf_mobile_phone_ed25519"
+if "%ETF_PHONE_HOST%"=="" set "ETF_PHONE_HOST=192.168.0.xxx"
+if "%ETF_PHONE_USER%"=="" set "ETF_PHONE_USER=termux_user"
+if "%ETF_PHONE_KEY%"=="" set "ETF_PHONE_KEY=%USERPROFILE%\.ssh\example_phone_ed25519"
 
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0deploy_to_phone.ps1" -Restart
 
